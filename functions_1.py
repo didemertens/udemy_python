11dbfbc57911602acccca5b93bb6071baa8aa96d
Dide Mertens <d.g.j.mertens@gmail.com>
2019-01-13 16:37:54 +0000

    added DOCSTRING changes

---
+++ functions_1.py
@@ -0,0 +1,9 @@
+def name_function():
+  '''
+  DOCSTRING: Information about the function.
+  Input: no inpt ...
+  Output: Hello...
+  '''
+  print('Hello')
+
+help(name_function) #shows DOCSTRING info
