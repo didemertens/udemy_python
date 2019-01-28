3ab0139f502c3be75ce63114324eb543fc4fa5d8
Dide Mertens <d.g.j.mertens@gmail.com>
2019-01-28 20:42:20 +0000

    Made bank account class

---
+++ bankaccount.py
@@ -0,0 +1,32 @@
+class Account:
+
+  def __init__(self, owner, balance):
+    self.owner = owner
+    self.balance = balance
+
+  def deposit(self, amount):
+    self.balance = self.balance + amount
+    print(f"Deposit of {amount} accepted.")
+
+  def withdraw(self, amount):
+    if self.balance >= amount:
+      self.balance = self.balance - amount
+      print(f"Withdrawal of {amount} accepted.")
+    else:
+      print("Funds unavailable!")
+
+  def __str__(self):
+    return f"Account owner: {self.owner}\nAccount balance: {self.balance}"
+
+
+acct1 = Account('Jose',100)
+print(acct1)
+print(acct1.owner)
+print(acct1.balance)
+acct1.deposit(50)
+print(acct1.balance)
+acct1.withdraw(75)
+print(acct1.balance)
+acct1.withdraw(500)
+print(acct1.balance)
+
