2096b93e04ddba2ad4bc6c43f0a85342e5f6c0bf
Dide Mertens <d.g.j.mertens@gmail.com>
2019-01-28 20:51:08 +0000

    Cleaned up the code a bit

--- bankaccount.py
+++ bankaccount.py
@@ -1,16 +1,16 @@
 class Account:
 
-  def __init__(self, owner, balance):
+  def __init__(self, owner, balance=0):
     self.owner = owner
     self.balance = balance
 
   def deposit(self, amount):
-    self.balance = self.balance + amount
+    self.balance += amount
     print(f"Deposit of {amount} accepted.")
 
   def withdraw(self, amount):
     if self.balance >= amount:
-      self.balance = self.balance - amount
+      self.balance -= amount
       print(f"Withdrawal of {amount} accepted.")
     else:
       print("Funds unavailable!")
