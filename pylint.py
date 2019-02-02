eca82cf607bb61f000e2596d591725d64e36cfd2
Dide Mertens <d.g.j.mertens@gmail.com>
2019-02-02 14:30:33 +0000

    Using pylint

---
+++ pylint.py
@@ -0,0 +1,17 @@
+"""
+A very simple script
+"""
+
+def myfunc():
+    """
+    A simple function
+    """
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
+
+
+# For full report Pylint use: pylint -ry pylint.py
