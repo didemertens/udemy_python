86a4d10c13ed159350d51eb7ceebda75d2ee571b
Dide Mertens <d.g.j.mertens@gmail.com>
2019-02-08 20:32:21 +0000

    Some more generators

---
+++ generators_homework.py
@@ -0,0 +1,29 @@
+def gensquares(N):
+  for x in range(N):
+    yield x ** 2
+
+
+#for x in gensquares(10):
+    #print(x)
+
+import random
+
+
+def rand_num(low, high, n):
+  for i in range(n):
+    yield random.randint(low,high)
+
+
+#for num in rand_num(1,10,12):
+#    print(num)
+
+s = 'hello'
+s = iter(s)
+print(next(s))
+
+my_list = [1,2,3,4,5,6]
+
+gencomp = (item for item in my_list if item > 3)
+
+for item in gencomp:
+  print(item)
