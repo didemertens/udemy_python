b148dc3391a98f9d5ad3ec35f715d9566c08923c
Dide Mertens <d.g.j.mertens@gmail.com>
2019-02-18 21:08:20 +0000

    datetime module, debugger module and timeit

---
+++ datetime_module.py
@@ -0,0 +1,22 @@
+import datetime
+
+t = datetime.time(5,25,1)
+print(t)
+t.minute
+
+print(datetime.time.min)
+print(datetime.time.max)
+
+
+today = datetime.date.today()
+print(today)
+
+print(today.timetuple())
+
+d1 = datetime.date(2015,3,11)
+print(d1)
+
+d2 = d1.replace(year=1990)
+print(d2)
+
+print(d1-d2)
---
+++ timeit_module.py
@@ -0,0 +1,8 @@
+import timeit
+
+'0-1-2-3-4-...-99'
+
+print(timeit.timeit('"-".join(str(n) for n in range(100))', number=10000))
+
+print(timeit.timeit('"-".join(map(str,range(100)))', number=10000))
+print("-".join(map(str,range(100))))
